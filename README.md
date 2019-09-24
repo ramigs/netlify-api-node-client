d8a95dfbf4174cbbccaa9a5aecd8b28a1da4d81f
ramigs <ramigs@gmail.com>
2019-09-24 12:53:31 +0100

    Getting GitHub Repo Id and Netlify createDeployKey() successfully. Still not getting flow control right

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # netlify-api-node-client
 Consuming Netlify's API using a Node.js console client
+
+## Setup
+
+> Install Node.js + npm on your system: https://nodejs.org/en/download/
+
+```sh
+$ git clone https://github.com/ramigs/netlify-api-node-client netlify-api-node-client
+$ cd netlify-api-node-client
+$ npm install
+$ npm test
+```
--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 require('dotenv').config()
-
 const NetlifyAPI = require('netlify')
-const client = new NetlifyAPI(process.env.API_KEY)
+const netlify = new NetlifyAPI(process.env.NETLIFY_API_KEY)
+const GitHub = require('github-api');
 
 
-async function getSites() {
-    console.log('calling');
-    var result = await client.listSites()
-    console.log(result);
+async function createDeployKey() {
+    console.log('Calling createDeployKey()...');
+    const result = await netlify.createDeployKey();
 }
 
-const sites = getSites();
+async function getSites() {
+    console.log('Calling getSites()...');
+    const result = await netlify.listSites()
+}
+
+async function createSite() {
+    console.log('Calling createSite()...');
+    // Create a site. Notice `body` here for sending OpenAPI body
+    //const site = await netlify.createSite(JSON.stringify({
+    const site = await netlify.createSite({
+        body: {
+            name: 'my-awesome-site'
+        }
+    });
+}
+
+// basic auth
+var gh = new GitHub({
+    //username: 'ramigs',
+    //password: '5WJTP6t8k3bXSqXz'
+    // also acceptable:
+    token: process.env.GITHUB_API_KEY
+
+});
+
+//var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
+/* me.listRepos(function(err, repos) {
+    console.log(repos);
+ 
+ 
+ }); */
+
+const repo = gh.getRepo('ramigs', 'javascript30');
+repo.getDetails()
+    .then((result) => console.log(result.data.id));
+
+
+//const sites = getSites();
+
+
+//createDeployKey();
+//createSite().catch(error => console.log(error));
+
+//console.log(deployKey);
+
+//createSite();
